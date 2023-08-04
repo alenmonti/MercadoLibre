c5127da875e16270f5c84d72d150d0f2e1f62b24
alenmonti <amonti@fi.uba.ar>
2023-08-04 09:15:33 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/htpps://github.com/alenmonti/MercadoLibre'
+  base: 'https://alenmonti.github.io/MercadoLibre/'
 })
