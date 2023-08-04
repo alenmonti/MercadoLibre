81d53044e1b9c1d22bf8fd8aacdf26199361965e
alenmonti <amonti@fi.uba.ar>
2023-08-04 12:49:27 -0300

    paymethods

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -4,8 +4,8 @@ import ProductItem from "./ProductItem";
 const ProductsContainer = ({ title, products, cartItems, setCartItems, productsFilter=""}) => {
 
     return (
-        <div id="Productos" className="max-w-[1400px] mx-auto my-8 lg:px-[100px]">
-            <h1 className="text-2xl font-semibold text-gray-700 my-4 text-center lg:text-left">{productsFilter ? productsFilter : title}</h1>
+        <div id="Productos" className="max-w-[1400px] mx-auto lg:px-[100px]">
+            <h1 className="text-2xl font-semibold text-gray-700 my-3 text-center lg:text-left">{productsFilter ? productsFilter : title}</h1>
             <div className="flex flex-wrap gap-5 justify-center lg:justify-between">
                 {products.map((product, index) =>
                     (productsFilter === "" || productsFilter === product.category) ? (
--- src/components/SideCart.jsx
+++ src/components/SideCart.jsx
@@ -9,10 +9,7 @@ import { HiArrowRight } from "react-icons/hi";
 
 const SideCart = ({ openCart, setOpenCart, cartItems, setCartItems }) => {
     return (
-        <aside
-            className={`fixed right-0 top-0 w-full h-full p-8 lg:w-[30%] bg-white z-50 transform ${
-                openCart ? "translate-x-0" : "translate-x-full"
-            } shadow-xl transition-transform duration-500 ease-in-out`}>
+        <aside className={`fixed right-0 top-0 w-full h-full p-8 lg:w-[30%] bg-white z-50 transform ${openCart ? "translate-x-0" : "translate-x-full"} shadow-xl transition-transform duration-500 ease-in-out`}>
             <div className="w-full h-[80px] flex justify-between items-center border-b-[1px] border-gray-300">
                 <h2 className="text-lg font-semibold">{`Cart (${cartItems.length})`}</h2>
                 <button onClick={() => setOpenCart(false)}>
@@ -32,30 +29,33 @@ const SideCart = ({ openCart, setOpenCart, cartItems, setCartItems }) => {
                             </div>
                             <div className="flex justify-between w-full text-xs font-semibold items-center">
                                 <div className="flex justify-between w-16 border-[1px] border-gray-300 px-[6px] py-1 text-sm font-normal rounded-sm">
-                                    <button onClick={() => {
-                                        if (product.amount - 1 <= 0) {
-                                            setCartItems((prev) => [...prev].filter((item) => item.id !== product.id));
-                                        } else {
-                                            setCartItems((prev) => {
-                                                return prev.map((item) => {
-                                                    if (item.id === product.id) return { ...item, amount: item.amount - 1};
-                                                    else return item;
+                                    <button
+                                        onClick={() => {
+                                            if (product.amount - 1 <= 0) {
+                                                setCartItems((prev) => [...prev].filter((item) => item.id !== product.id));
+                                            } else {
+                                                setCartItems((prev) => {
+                                                    return prev.map((item) => {
+                                                        if (item.id === product.id) return { ...item, amount: item.amount - 1 };
+                                                        else return item;
+                                                    });
                                                 });
-                                            });
-                                        }}}
-                                    className="w-4">
+                                            }
+                                        }}
+                                        className="w-4">
                                         <BsDash className="mx-auto" />
                                     </button>
                                     <p>{product.amount}</p>
-                                    <button onClick={() => {
-                                        setCartItems((prev) => {
-                                            return prev.map((item) => {
-                                                if (item.id === product.id) return { ...item, amount: item.amount + 1};
-                                                else return item;
+                                    <button
+                                        onClick={() => {
+                                            setCartItems((prev) => {
+                                                return prev.map((item) => {
+                                                    if (item.id === product.id) return { ...item, amount: item.amount + 1 };
+                                                    else return item;
+                                                });
                                             });
-                                        });
-                                    }}
-                                    className="w-4">
+                                        }}
+                                        className="w-4">
                                         <BsPlus className="mx-auto" />
                                     </button>
                                 </div>
