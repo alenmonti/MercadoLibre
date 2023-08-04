21058f4eefee2b64514dd8ff75251a7a0b3d1575
alenmonti <amonti@fi.uba.ar>
2023-08-04 12:35:26 -0300

    Update PayMethods.jsx

--- src/components/PayMethods.jsx
+++ src/components/PayMethods.jsx
@@ -3,13 +3,15 @@ import PayMethod from "./PayMethod";
 
 const PayMethods = () => {
     return (
-        <div className="bg-[#ffffff] flex  mx-auto w-max py-5 px-2 gap-2 my-[40px] justify-evenly rounded-md shadow-md min-w-[1200px] items-center overflow-x-scroll scroll">
+        <div className="w-full overflow-x-scroll scroll">
+        <div className="bg-[#ffffff] flex  mx-auto w-max py-5 px-2 gap-2 my-[40px] justify-evenly rounded-md shadow-md min-w-[1200px] items-center">
             <PayMethod img="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/credit-card.svg" type="Tarjeta de crédito" link="Ver promociones bancarias"/>
             <PayMethod img="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/debit-card.svg" type="Tarjeta de débito" link="Ver más"/>
             <PayMethod img="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/mercado-creditsv2.svg" type="Cuotas sin tarjeta" link="Conocé Mercado Crédito"/>
             <PayMethod img="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/payment-agreement.svg" type="Efectivo" link="Ver más"/>
             <div className="h-full w-[1px] border-gray-200 border-l-[1px] mx-3"></div>
             <PayMethod img="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/view-more.svg" type="" link="" center={true}/>
+        </div>
         </div>
     );
 };
